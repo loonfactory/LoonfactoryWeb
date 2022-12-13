6ad8c3356d2f38bfa51c362d2d9435b36f7bd533
loonfactory <thsdnjstjq1@naver.com>
2022-12-13 22:55:19 +0900

    초기 커밋.

---
+++ LoonfactoryPage/Data/LoonfactoryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoonfactoryPage.Data
+{
+    public class LoonfactoryDbContext : IdentityDbContext
+    {
+        public LoonfactoryDbContext(DbContextOptions<LoonfactoryDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
